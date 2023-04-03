348056e36e17fa50ac5599c65f32810c168d9d4f
Geralt-Of-Rivia-Witcher <singhsiddhantkumar@gmail.com>
2023-04-04 01:01:21 +0530

    fix: :bug: Missing export in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import RabbitMq from './utils/mqconfig';
+import Emailer from './utils/emailerConfig';
 import { ConsumeMessage, Channel } from 'amqplib';
-export { RabbitMq, ConsumeMessage, Channel };
+export { RabbitMq, ConsumeMessage, Channel, Emailer };
