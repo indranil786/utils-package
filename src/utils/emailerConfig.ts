bff38cd4a329ee592ef56de3308a1c07e4a0ddac
Geralt-Of-Rivia-Witcher <singhsiddhantkumar@gmail.com>
2023-06-01 00:44:01 +0530

    feat: :sparkles: Send HTML Email

--- src/utils/emailerConfig.ts
+++ src/utils/emailerConfig.ts
@@ -2,18 +2,20 @@ import {
   SESClient,
   SendTemplatedEmailCommand,
   SendTemplatedEmailCommandOutput,
+  SendEmailCommand,
+  SendEmailCommandOutput,
 } from '@aws-sdk/client-ses';
 
 export default class Emailer {
   sesClient: SESClient;
 
-  constructor(region: string, accessKey : string, secretKey : string) {
+  constructor(region: string, accessKey: string, secretKey: string) {
     this.sesClient = new SESClient({
       region: region,
       credentials: {
         accessKeyId: accessKey,
         secretAccessKey: secretKey,
-      }
+      },
     });
   }
 
@@ -42,6 +44,45 @@ export default class Emailer {
         TemplateData: JSON.stringify(templateData),
         Source: sourceEmailAddress,
         Template: templateName,
+      })
+    );
+  }
+
+  /**
+   *
+   * @param {string | Array<string>} destinationEmailAddress
+   * @param {string} HTMLBody
+   * @param {string} subject
+   * @param {string} sourceEmailAddress
+   * @returns {Promise<SendEmailCommandOutput>}
+   */
+  async sendHTMLEmail(
+    destinationEmailAddress: string | string[],
+    HTMLBody: string,
+    subject: string,
+    sourceEmailAddress: string
+  ): Promise<SendEmailCommandOutput> {
+    return await this.sesClient.send(
+      new SendEmailCommand({
+        Destination: {
+          ToAddresses:
+            typeof destinationEmailAddress === 'string'
+              ? [destinationEmailAddress]
+              : destinationEmailAddress,
+        },
+        Message: {
+          Body: {
+            Html: {
+              Charset: 'UTF-8',
+              Data: HTMLBody,
+            },
+          },
+          Subject: {
+            Charset: 'UTF-8',
+            Data: subject,
+          },
+        },
+        Source: sourceEmailAddress,
       })
     );
   }
